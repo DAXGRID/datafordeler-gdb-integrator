48735f6633815b7d6607f87920bd2e8c1e038c2e
Mihai <mihai.tirtara@gmail.com>
2020-10-23 11:54:41 +0200

    Interface created for postgres integration

---
+++ Datafordeler.GDBIntegrator/Database/IPostgresWriter.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+using Newtonsoft.Json.Linq;
+namespace Datafordeler.GDBIntegrator.Database
+{
+    public interface IPostgresWriter
+    {
+        void createTable(string topic, string[] columns);
+        bool checkTable(string topic);
+
+        List<JObject> checkLatestDataDuplicates(List<JObject> batch);
+
+        void UpsertData(List<JObject> batch, string topic,string[] columns);
+         void AddToPSQL(List<JObject> batch, string topic,string[] columns);
+         
+    }
+
+}
